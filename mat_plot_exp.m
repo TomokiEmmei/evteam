a356934c2c4e7c2f3a6b300708ae9defc9ce017e
TomokiEmmei <enmei14@hflab.k.u-tokyo.ac.jp>
2018-04-24 15:36:46 +0900

    eliminate plots which does not have variables in workspace

--- mat_plot_exp.m
+++ mat_plot_exp.m
@@ -1,13 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%【宿題】
+%　・フォルダ内の全ファイル読み込み機能をつける
+%　・struct_nameがダメすぎる
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 close all;
 clear all;      % これはclear allしないと動かないよ
 
-%saveするか否か
+%%
+% saveするか否か
  save_flag=1;
 
 % データファイルのロード
-filename='180423test1';
+filename='180423lfs_test2';
 dirname='180423data';
 dir_file_name=sprintf('raw/%s/%s', dirname, filename);
+%dir(sprintf('raw/%s', dirname));
 load(dir_file_name);
 who_array=who; % work space内の変数リストを配列に格納
 struct_name = char(who_array(5));        % clear allしていればワークスペース変数は構造体だけ
@@ -69,80 +78,102 @@ if save_flag==1
     if  exist(savedir) ~=7
         mkdir(savedir)
     end
-savename=sprintf('expand/%s/%s_expand', dirname, filename);
+savename=sprintf('expand/%s/%s_expand', dirname, struct_name);
 save(savename);
 clear savename dirname filename;
 end
 
 %% データの保存とプロット
-%Yawのプロット
 xmin = 0;
 xmax = 10;
 
-hfig = figure;%('Position',[50 50 450 350]);
-plot(time, gamma_LPF+2.18, 'r', 'linewidth',3); hold on;
-xlabel('Time [s]');
-ylabel('Yaw rate [rad/s]');
-pfig = pubfig(hfig);
-pfig.LegendLoc = 'best';
-xlim([xmin, xmax]);
+%Yaw rate
+if  exist('gamma') ==1
+    hfig = figure;%('Position',[50 50 450 350]);
+    plot(time, gamma_LPF+2.18, 'r', 'linewidth',3); hold on;
+    xlabel('Time [s]');
+    ylabel('Yaw rate [rad/s]');
+    pfig = pubfig(hfig);
+    pfig.LegendLoc = 'best';
+    xlim([xmin, xmax]);
 
-if save_flag==1
-savename = sprintf('fig/%s_gamma',struct_name);
-expfig(savename,'-pdf','-emf','-png','-fig');
+    if save_flag==1
+        savename = sprintf('fig/%s_gamma',struct_name);
+        expfig(savename,'-png'); %,'-pdf','-emf','-png','-fig'
+    end
 end
 
-%前輪車輪角速度のプロット
-hfig = figure;
-xlabel('Time [sec]');
-ylabel('Angular velocity [rad/s]');
-plot(time, w_front_left, 'r', 'linewidth',3); hold on;
-plot(time, w_front_right, 'b --', 'linewidth',3);
-legend('FL','FR',1);
-%title('Vehicle speed');
-pfig = pubfig(hfig);
-pfig.LegendLoc = 'best';
-xlim([xmin, xmax]);
-
-if save_flag==1
-savename = sprintf('fig/%s_w_f',struct_name);
-expfig(savename,'-pdf','-emf','-png','-fig');
+% Wheel angular velocity
+if  [exist('w_front_left'), exist('w_front_right'), exist('w_rear_left'), exist('w_rear_right')] ==[1, 1, 1, 1]
+    hfig = figure;
+    xlabel('Time [sec]');
+    ylabel('Angular velocity [rad/s]');
+    plot(time, w_front_left, 'r', 'linewidth',3); hold on;
+    plot(time, w_front_right, 'b --', 'linewidth',3);
+    plot(time, w_rear_left, 'k', 'linewidth',3); 
+    plot(time, w_rear_right, 'g --', 'linewidth',3);
+    legend('fl', 'fr', 'rl', 'rr', 1);
+    pfig = pubfig(hfig);
+    pfig.LegendLoc = 'best';
+    xlim([xmin, xmax]);
+    if save_flag==1
+        savename = sprintf('fig/%s_w',struct_name);
+        expfig(savename,'-png');
+    end
 end
 
-%後輪車輪角速度のプロット
-hfig = figure;
-xlabel('Time [sec]');
-ylabel('Angular velocity [rad/s]');
-plot(time, w_rear_left, 'k', 'linewidth',3); hold on;
-plot(time, w_rear_right, 'g --', 'linewidth',3);
-legend('RL','RR',1);
-%title('Vehicle speed');
-pfig = pubfig(hfig);
-pfig.LegendLoc = 'best';
-xlim([xmin, xmax]);
-
-if save_flag==1
-savename = sprintf('fig/%s_w_r',struct_name);
-expfig(savename,'-pdf','-emf','-png','-fig');
+% Lateral force sensor
+if  [exist('Fy_fl'), exist('Fy_fr'), exist('Fy_rl'), exist('Fy_rr')] ==[1, 1, 1, 1]
+    hfig = figure;
+    plot(time, Fy_fl, 'r', 'linewidth',3); hold on;
+    plot(time, Fy_fr, 'b.-', 'linewidth',3);
+    plot(time, Fy_rl, 'k--', 'linewidth',3);
+    plot(time, Fy_rr, 'g--', 'linewidth',3);
+    legend('fl', 'fr', 'rl', 'rr', 1, 'Location','northeast');
+    xlabel('Time [sec]');
+    ylabel('Torque [Nm]');
+    pfig = pubfig(hfig);
+    pfig.LegendLoc = 'best';
+    xlim([xmin, xmax]);
+    % ylim([ymin, ymax]);
+    if save_flag==1
+        savename = sprintf('fig/%s_torque',struct_name);
+        expfig(savename,'-pdf','-emf','-png');
+    end
 end
 
-%入力トルクのプロット
-% ymin = -400;
-% ymax = 400;
-hfig = figure;
-plot(time, trq_real_front_left, 'r', 'linewidth',3); hold on;
-plot(time, trq_real_front_right, 'b.-', 'linewidth',3);
-plot(time, trq_real_rear_left, 'k--', 'linewidth',3);
-plot(time, trq_real_rear_right, 'g--', 'linewidth',3);
-legend('FL', 'FR', 'RL', 'RR', 1, 'Location','northeast');
-xlabel('Time [sec]');
-ylabel('Torque [Nm]');
-pfig = pubfig(hfig);
-pfig.LegendLoc = 'best';
-xlim([xmin, xmax]);
-% ylim([ymin, ymax]);
-if save_flag==1
-savename = sprintf('fig/%s_torque',struct_name);
-expfig(savename,'-pdf','-emf','-png');
+% Encoder value of EPS
+if  [exist('rear_eps_enc'), exist('front_eps_enc')] ==[1, 1]
+    hfig = figure;
+    xlabel('Time [sec]');
+    ylabel('EPS angle [rad]');
+    plot(time, front_eps_enc, 'r', 'linewidth',3); hold on;
+    plot(time, rear_eps_enc, 'b --', 'linewidth',3);
+    legend('front', 'rear', 1);
+    pfig = pubfig(hfig);
+    pfig.LegendLoc = 'best';
+    xlim([xmin, xmax]);
+    if save_flag==1
+        savename = sprintf('fig/%s_eps_enc',struct_name);
+        expfig(savename,'-png');
+    end
 end
 
+% EPS torque
+if  [exist('eps_trq_f'), exist('eps_trq_r')] ==[1, 1]
+    hfig = figure;
+    xlabel('Time [sec]');
+    ylabel('EPS torquw [Nm]');
+    plot(time, eps_trq_f, 'r', 'linewidth',3); hold on;
+    plot(time, eps_trq_r, 'b --', 'linewidth',3);
+    legend('front', 'rear', 1);
+    pfig = pubfig(hfig);
+    pfig.LegendLoc = 'best';
+    xlim([xmin, xmax]);
+    if save_flag==1
+        savename = sprintf('fig/%s_eps_trq',struct_name);
+        expfig(savename,'-png');
+    end
+end
+
+close all;
